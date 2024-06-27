a3f414cb9b1c1f1f32cfb6be722f46eb94281456
macbook m1 <dungdv01852@gmail.com>
2024-06-27 11:02:53 +0700

    gg

--- src/server.js
+++ src/server.js
@@ -1,25 +1,25 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import viewEngine from './configs/viewEngine'
-import webRoutes from './routes/web'
-import localtunnel from 'localtunnel'
+import express from "express";
+import bodyParser from "body-parser";
+import viewEngine from "./configs/viewEngine";
+import webRoutes from "./routes/web";
+import localtunnel from "localtunnel";
 
-let app = express()
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+let app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-let port = process.env.PORT || 8080
-let localtunnelPort = 8081
+let port = process.env.PORT || 8080;
+let localtunnelPort = 8888;
 
 //config view engine
 // viewEngine(app)
 
 //config web routes
-webRoutes(app)
+webRoutes(app);
 
 app.listen(localtunnelPort, () => {
-    console.log('express ready')
-})
+  console.log("express ready");
+});
 
 // localtunnel({
 //     subdomain: 'messengerbot',
@@ -33,19 +33,19 @@ app.listen(localtunnelPort, () => {
 
 // Verify that the callback came from Facebook.
 const verifyRequestSignature = (req, res, buf) => {
-    var signature = req.headers["x-hub-signature"];
+  var signature = req.headers["x-hub-signature"];
 
-    if (!signature) {
-        console.warn(`Couldn't find "x-hub-signature" in headers.`);
-    } else {
-        var elements = signature.split("=");
-        var signatureHash = elements[1];
-        var expectedHash = crypto
-            .createHmac("sha1", config.appSecret)
-            .update(buf)
-            .digest("hex");
-        if (signatureHash != expectedHash) {
-            throw new Error("Couldn't validate the request signature.");
-        }
+  if (!signature) {
+    console.warn(`Couldn't find "x-hub-signature" in headers.`);
+  } else {
+    var elements = signature.split("=");
+    var signatureHash = elements[1];
+    var expectedHash = crypto
+      .createHmac("sha1", config.appSecret)
+      .update(buf)
+      .digest("hex");
+    if (signatureHash != expectedHash) {
+      throw new Error("Couldn't validate the request signature.");
     }
-}
+  }
+};
